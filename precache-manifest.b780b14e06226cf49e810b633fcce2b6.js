4e5c10dc6fccc2cc9f1be8075724d8800ccbe79d
Godfrey <ezechibueze3@gmail.com>
2020-09-05 18:28:52 +0100

    Updates

---
+++ precache-manifest.b780b14e06226cf49e810b633fcce2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c90f6d7b29b8bfd719b00af082831d",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "a1d5d01ceaddccac0dab",
+    "url": "/Monster-Rolodex/static/css/main.48875664.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/Monster-Rolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d5d01ceaddccac0dab",
+    "url": "/Monster-Rolodex/static/js/main.b15db866.chunk.js"
+  },
+  {
+    "revision": "3ca3d2aacf82f3812c9e",
+    "url": "/Monster-Rolodex/static/js/runtime-main.c58f4534.js"
+  }
+]);
